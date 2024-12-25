a2b3d9899473dae8986a35a6967bcaf7bb8bc84b
Denzel Ashitey <niiarmahdenzel@gmail.com>
2024-12-25 21:44:00 +0000

    UI development

---
+++ script.js
@@ -0,0 +1,16 @@
+const BRUSH = document.querySelector('.brush')
+const BUCKET = document.querySelector('.bucket')
+const ERASER = document.querySelector('.eraser')
+const SIZEBTN = document.querySelector('.set-size-btn')
+const GRIDSIZE = document.querySelector('.grid-size')
+
+// create a function that would create a div and then append a number of grid sizes to it using the value of gridSize.value
+
+// declare function generateGrid
+// create an empty div inside it
+// use the value of the gridSize parameter to set a size for the grid
+// generate grids using flex box to correspond with the number of grids specified
+SIZEBTN.addEventListener('click', () => {
+    
+})
+
