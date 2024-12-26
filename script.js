c926f7ec02a35b9fa1b98eabfba481a30604848e
Denzel Ashitey <niiarmahdenzel@gmail.com>
2024-12-26 11:55:35 +0000

    implemented grid generation system

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const BRUSH = document.querySelector('.brush')
 const BUCKET = document.querySelector('.bucket')
 const ERASER = document.querySelector('.eraser')
 const SIZEBTN = document.querySelector('.set-size-btn')
-const GRIDSIZE = document.querySelector('.grid-size')
+let gridSize = document.querySelector('.grid-size')
+let drawingSpace = document.querySelector('.drawing-space')
 
-// create a function that would create a div and then append a number of grid sizes to it using the value of gridSize.value
 
-// declare function generateGrid
-// create an empty div inside it
-// use the value of the gridSize parameter to set a size for the grid
-// generate grids using flex box to correspond with the number of grids specified
 SIZEBTN.addEventListener('click', () => {
-    
+    let gridboxes = gridSize.value;
+    let gridCount = Math.pow(gridboxes, 2);
+    let gridBoxSize = 100 / gridboxes;
+    for (let i = 0; i < gridCount; i++) {
+        let grids = document.createElement('div')
+        grids.classList.add('gridbox')
+        grids.style.width = `calc(100% / ${gridboxes})`
+        grids.style.width = `calc(100% / ${gridboxes})`
+        drawingSpace.appendChild(grids);
+    } 
 })
-
