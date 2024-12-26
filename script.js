01642ac076a02defa853adabe2821c99648e3a0e
Denzel Ashitey <niiarmahdenzel@gmail.com>
2024-12-26 16:45:02 +0000

    added grid toggle system

--- script.js
+++ script.js
@@ -2,19 +2,31 @@ const BRUSH = document.querySelector('.brush')
 const BUCKET = document.querySelector('.bucket')
 const ERASER = document.querySelector('.eraser')
 const SIZEBTN = document.querySelector('.set-size-btn')
+const TOGGLEGRIDBTN = document.querySelector('.grid-btn-container')
 let gridSize = document.querySelector('.grid-size')
 let drawingSpace = document.querySelector('.drawing-space')
-
 
 SIZEBTN.addEventListener('click', () => {
     let gridboxes = gridSize.value;
     let gridCount = Math.pow(gridboxes, 2);
-    let gridBoxSize = 100 / gridboxes;
     for (let i = 0; i < gridCount; i++) {
         let grids = document.createElement('div')
         grids.classList.add('gridbox')
         grids.style.width = `calc(100% / ${gridboxes})`
         grids.style.width = `calc(100% / ${gridboxes})`
         drawingSpace.appendChild(grids);
+        ERASER.addEventListener('click', ()=> {
+            grids.addEventListener('mouseover', ()=> {
+                grids.style.backgroundColor = 'white';
+            })
+        })
+        BRUSH.addEventListener('click', ()=> {
+            grids.addEventListener('mouseover', ()=> {
+                grids.style.backgroundColor = 'black';
+            })
+        })
+        TOGGLEGRIDBTN.addEventListener('click', ()=> {
+            grids.classList.toggle('gridbox-grids')
+        })
     } 
 })
