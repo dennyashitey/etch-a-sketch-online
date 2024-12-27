70e98b9eac5f6b890c23dfc39e8fe4bced72954a
Denzel Ashitey <niiarmahdenzel@gmail.com>
2024-12-27 11:49:37 +0000

    Share btn bug fixes

--- script.js
+++ script.js
@@ -14,33 +14,46 @@ HELPBTN.addEventListener('click', () => {
 });
 
 SIZEBTN.addEventListener('click', () => {
+    // clear the drawing space
     drawingSpace.innerHTML = '';
-    let gridboxes = gridSize.value;
-    let gridCount = Math.pow(gridboxes, 2);
+    let gridboxes = parseInt(gridSize.value);
+    // loop to check if value of gridsize is a mult of 4
+    if (gridboxes % 4 == 0 ) {
+        let gridCount = Math.pow(gridboxes, 2); 
     for (let i = 0; i < gridCount; i++) {
+        //create a grid 
         let grids = document.createElement('div')
         grids.classList.add('gridbox')
         grids.style.width = `calc(100% / ${gridboxes})`
         grids.style.width = `calc(100% / ${gridboxes})`
+        //append the grid to the drawing space
         drawingSpace.appendChild(grids);
+        // change background color of grid to white with erase tool
         ERASER.addEventListener('click', ()=> {
             grids.addEventListener('mouseover', ()=> {
                 grids.style.backgroundColor = 'white';
             })
         })
+        //double click event to change all grids to white
         ERASER.addEventListener('dblclick', ()=> {
             const allGrids = document.querySelectorAll('.gridbox');
             allGrids.forEach(grid => {
                 grid.style.backgroundColor = 'white'
             });
         })
+        //activate the color change hover
         BRUSH.addEventListener('click', ()=> {
             grids.addEventListener('mouseover', ()=> {
                 grids.style.backgroundColor = `${colorPicker.value}`;
             })
         })
+        // toggle grid boxes on or off
         TOGGLEGRIDBTN.addEventListener('click', ()=> {
             grids.classList.toggle('gridbox-grids')
         })
     } 
+    } else {
+        alert('Grid size should be a multiple of 4')
+    }
+    
 })
